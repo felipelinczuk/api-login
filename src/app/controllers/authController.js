d9f7681f744b5759564699fc9c4d35c4020b170f
Felipe Linczuk <felipelinczuk@outlook.com>
2021-10-14 12:22:56 -0300

    fixing important bugs

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -5,13 +5,15 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const authconfig = require('../../config/auth.json')
 const crypto = require('crypto')
+const mailer = require('../../modules/mailer.js')
 
 function generateToken(params={}){
     return jwt.sign(params, authconfig.secret, {expiresIn: 86400});  
-}
+};
 
 router.post('/register', async(req, res) => {
     const {email} = req.body
+    console.log(email)
     try{
         if(await User.findOne({email}))
             return res.status(400).send({error: 'E-mail is already used'});
@@ -21,13 +23,15 @@ router.post('/register', async(req, res) => {
         return res.send({user, token: generateToken({id: user.id})});
     }
     catch(err){
+        console.log(err)
         return res.status(400).send({error: 'Registration failed'});
     }
-})
+});
 
 router.post('/authenticate', async(req, res) => {
-    const {email,password} = req.body
+    const {email, password} = req.body
     const user = await User.findOne({email}).select('+password')
+
     if(!user)
         return res.status(400).send({error: 'User not found'});
     
@@ -37,7 +41,7 @@ router.post('/authenticate', async(req, res) => {
       
     user.password = undefined    
     res.send({user, token: generateToken({id: user.id})});
-})
+});
 
 router.post('/forgot_password', async (req, res) => {
     const {email} = req.body
@@ -61,12 +65,55 @@ router.post('/forgot_password', async (req, res) => {
             }
         })
 
-        console.log(token, expires)
+        mailer.sendMail({
+            to: email,
+            from: 'noreply-password@services.com',
+            template: 'auth/forgot_password',
+            context: {token}
+
+        }, (err) => {
+            if(err){
+                console.log(err)
+                return res.status(400).send({error: 'Cannot send reset password e-mail'});
+            }   
+
+            return res.send();
+        })
+        
     }
     catch(err){
         res.status(400).send({ error: `Error! Try again. Details: ${err}`})
     }
-})
+});
+
+router.post('/reset_password', async(req, res) => {
+    const {email, token, password} = req.body
+
+    try{
+        const user = await User.findOne({email})
+            .select('+passwordResetToken passwordResetExpires')
+        
+        if(!user)
+            return res.status(400).send({error: 'User not found'})
+
+        if(token !== user.passwordResetToken)
+            return res.status(400).send({error: 'Invalid token'})
+
+        const now = new Date()
+
+        if(now > user.passwordResetExpires)
+            return res.status(400).send({error: 'Expired token'})
+
+
+        user.password = password
+
+        await user.save()
+        res.send();
+    }
+    catch(err){
+        return res.status(400).send({error: 'Cannot reset password'})
+    }
+});
 
 module.exports = (app) => app.use('/auth', router);
 
